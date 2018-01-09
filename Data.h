b623c0c70d643e27ed35dbac64fedfbd9fbcdc59
Abdirahman Mohamed <32283283+mohamedabo@users.noreply.github.com>
2018-01-09 05:01:13 -0500

    Add files via upload

---
+++ Data.h
@@ -0,0 +1,37 @@
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+#include <cstdio>
+#include <vector>
+#include <string>
+
+class Data {
+private:
+    std::string html;
+    std::string sign;
+    std::string name;
+    double low;
+    double high;
+    double current;
+    double chg;
+    double pChg;
+
+public:
+    /**
+     * @brief Constructor for a Data object.
+     *
+     * @param NAME The ticker of the company.
+     * @param HTML The html to pull info from.
+     */
+    Data (std::string NAME, std::string HTML);
+
+    /**
+     * @brief Picks through the html to find the needed data.
+     */
+    void fillData();
+
+    /**
+     * @brief Prints out all the stock data for a company.
+     */
+    void printData();
+};
+#endif // DATA_H_INCLUDED
